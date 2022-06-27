378ce7ce91e022591ff5d77d3514dc82cb8202b6
Milton Ouma <ouma.nyang1@gmail.com>
2022-06-27 20:03:25 +0300

    Project completed

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -5,20 +5,16 @@
 * Copyright 2022 Ouma Nyang
 * Github - OumaNyang
 * Open Project Licensed under MIT
-
 */
-
 let searchBtn = document.getElementById("search-btn");
 let countryInp = document.getElementById("country-inp");
 searchBtn.addEventListener("click", () => {
     document.getElementById("loader").classList.add('loader');
     document.getElementById("search-btn").style.display = 'none';
 ;
-
-  let countryName = countryInp.value;
+  let countryName = countryInp.value.toUpperCase();
   let finalURL = `https://restcountries.com/v3.1/name/${countryName}?fullText=true`;
   
-
   fetch(finalURL)
     .then((response) => response.json())
     .then((data) => {
@@ -26,13 +22,23 @@ searchBtn.addEventListener("click", () => {
         document.getElementById("search-btn").style.display = 'block';
 
       result.innerHTML = `
-      <table class="table table-sm table-bordered table-striped">
-    <tr>
-        <td class="text-center" colspan="2">${data[0].name.common}</td>
-      </tr>
-    <tr>
+      <table class="table table-sm table-bordered table-striped table-reponsive">
+   <tr>
         <td class="text-center" colspan="2"><img src="${data[0].flags.svg}" class="flag-img"></td>
       </tr>
+
+      <tr>
+      <td  id="countrydetails" class="text-center" colspan="2">${(data[0].name.common).toUpperCase()}</td>
+    </tr>
+    <tr>
+    <td>Official Names </td>
+    <td><h2 class="text-center">${data[0].name.official}<h2></td>
+  </tr>
+  
+  <tr>
+  <td> Coat of arms </td>
+  <td><img width="50px" src="${data[0].coatOfArms.svg}" class="coa-img"></td>
+</tr>
     <tr>
         <td>Capital</td>
         <td>${data[0].capital[0]}</td>
@@ -47,9 +53,7 @@ searchBtn.addEventListener("click", () => {
       </tr>
        <tr>
         <td>Currency</td>
-        <td>${
-  data[0].currencies[Object.keys(data[0].currencies)].name
-} - ${Object.keys(data[0].currencies)[0]}</td>
+        <td>${ data[0].currencies[Object.keys(data[0].currencies)].name} - ${Object.keys(data[0].currencies)[0]}</td>
        </tr>
        <tr>
         <td>Common Languages</td>
@@ -59,44 +63,68 @@ searchBtn.addEventListener("click", () => {
   .join(", ")}</td>
         </tr>
         <tr>
-        <td>Currency</td>
-     <td> </td>
+        <td>Timezone</td>
+     <td>${data[0].timezones} </td>
        <tr>
-        <td>Currency</td>
-        <td>2</td>
+        <td>TLD</td>
+        <td>${data[0].tld}</td>
       </tr>
         <tr>
-        <td class="text-center" colspan="2"><button onclick="addFavourite(${data[0].name.common})" class="btn btn-sm btn-success rounded-0">Add Favourite</button></td>
+        <td class="text-center" colspan="2"><button id="addFav" class="btn btn-sm btn-success rounded-0">Add Favourite</button></td>
       </tr>
   </table> `;
+
+  let  addFav =document.getElementById('addFav')
+  let  removeFav =document.getElementById('removeFav')
+
+  addFav.addEventListener("click", () => {
+    addFavourite(countryName)
+  })
+
+
     })
     .catch(() => {
       if (countryName.length == 0) {
         document.getElementById("loader").classList.remove('loader');
         document.getElementById("search-btn").style.display = 'block';
-      setTimeout(() => {  result.innerHTML = `<div class="alert alert-danger">The input field cannot be empty</div>`}, 3000);
+      result.innerHTML = `<div class="alert alert-danger">The input field cannot be empty</div>`;
+      setTimeout(function() {result.close();}, 5000)
+
       } else {
         document.getElementById("loader").classList.remove('loader');
         document.getElementById("search-btn").style.display = 'block';
-       setTimeout(() => {   result.innerHTML = `<div class="alert alert-danger">Please enter a valid country name or check your internet connection<div>` }, 3000);
+        result.innerHTML = `<div class="alert alert-danger">Please enter a valid country name or check your internet connection<div>`;
       }
     });
+
 });
 
-let countryName;
 function addFavourite(countryName){
-
 let favContainer =  document.getElementById('favList')
-//const heading_text = document.createTextNode("Kenya");
+const node = document.createElement("li");
+const span = document.createElement("span");
 
-favContainer.appendChild(countryName);
+const textnode = document.createTextNode(countryName);
+span.setAttribute('id','star');
+node.appendChild(textnode);
+node.appendChild(span);
+  favContainer.appendChild(node);
+  
+  }
 
-//alert(' Hi there' );
 
-}
+  window.addEventListener("load", () => {
+    if(navigator.onLine) {
+     
+      result.innerHTML = `<div class="alert alert-success">Internet connection  available.Proceeed with your search<div>` 
+      document.getElementById("search-btn").style.display = 'block';
 
-// window.addEventListener("load", () => {
+     } else {
+       result.innerHTML = `<div class="alert alert-danger">Oops! You're offline. Please check your network connection...<div>`;
+       document.getElementById("search-btn").style.display = 'none';
 
-//     addFavourite()
+     }
+  })
 
-// });
+
+  
